7cc6958b21da2c7f0642ee5e6336818c41affc02
imb1006 <imb1006@alu.ubu.es>
2021-11-10 11:35:53 +0100

    Subida de datos de suicidio y sectores de empleo por países. Corregir archivo de sectores.

--- Suicidio-clima-y-desarrollo-global.R
+++ Suicidio-clima-y-desarrollo-global.R
@@ -1,8 +1,16 @@
 # Importación de Datos ----------------------------------------------------
+library(readr)
 
 # Para tener los datos del clima utilizamos importacion de csv utilizando las opciones: import dataset from text (readr)
-library(readr)
 Datos_Clima <- read_csv("INPUT/DATA/GlobalLandTemperaturesByCountry.csv")
-Datos_Clima #Vemos que los datos son correctas
+Datos_Clima #Vemos que los datos son correctos
+# Habría que elegir las fechas correctas porque contiene todos los datos desde 1700 al 2013
 
-# Habría que elegir las fechas correctas porque ahora mismo tenemos todos los datos desde 1700 al 2013
+# Carga de datos de suicidio desde el 2000 al 2019:
+Datos_Suicidio <- read_csv("INPUT/DATA/SuicideGlobalRates.csv")
+Datos_Suicidio
+# La tasa media de suicidios por 100.000 habitantes se encuentra en la columna "FactValueNumeric"
+
+# Carga de datos de los diferentes sectores de empleo por países desde el 2000 al 2013:
+# NO ESTÁ BIEN CARGADO, CORREGIR!!!!!
+Datos_Sectores <- read_csv("INPUT/DATA/EmploymentSectors.csv")
